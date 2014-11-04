9e0abe61fbbbd4c909ebb0e4cac1f5ef162ee940
starzou <starzou@126.com>
2014-11-04 22:52:02 +0800

    添加 Angular 示例

---
+++ app/technologies/angular/app.js
@@ -0,0 +1,22 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ *@class app
+ *@description
+ *@time 2014-11-04 22:45
+ *@author StarZou
+ **/
+'use strict';
+
+// 定义模块
+var App = angular.module('App', []);
+
+// 在模块上 注册controller
+App.controller('mainCtrl', ['$scope', function ($scope) {
+    $scope.word = 'Hello Angular!';
+}]);
