1162569a7d45546e7a0aeafc4c7d3a43fad3e74a
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-11-12 16:02:35 +0800

    angular 示例

--- app/technologies/angular/app.js
+++ app/technologies/angular/app.js
@@ -13,13 +13,17 @@
  **/
 'use strict';
 
-// 定义模块
+/**
+ * 定义模块
+ */
 var App = angular.module('App', []);
 
-// 在模块上 注册controller
+/**
+ * 注册controller
+ */
 App.controller('appCtrl', ['$scope' , '$filter', function ($scope, $filter) {
 
-    $scope.word = 'Hello ';
+    $scope.word = 'hello ';
 
     $scope.date = new Date();
 
@@ -31,10 +35,26 @@ App.controller('appCtrl', ['$scope' , '$filter', function ($scope, $filter) {
     $scope.users = [
         {id: 1, name: 'star1'},
         {id: 2, name: 'star2'},
-        {id: 3, name: 'star3'},
+        {id: 3, name: 'star3'}
     ];
 
     $scope.addUser = function (name) {
         $scope.users.push({id: Date.now(), name: name});
-    }
+        $scope.name = undefined;
+    };
+
+    $scope.removeUser = function (index) {
+        $scope.users.splice(index, 1);
+    };
 }]);
+
+/**
+ * 注册自定义过滤器
+ */
+App.filter('capitalize', function () {
+    return function (input) {
+        if (input) {
+            return input[0].toUpperCase() + input.slice(1);
+        }
+    };
+});
