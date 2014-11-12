d810155a0f46e2e9ee70d4834b75d34c1cec9a55
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-11-12 18:35:23 +0800

    angular form 示例

--- app/technologies/angular/app.js
+++ app/technologies/angular/app.js
@@ -58,3 +58,49 @@ App.filter('capitalize', function () {
         }
     };
 });
+
+App.directive('validateForm', function () {
+    return {
+        require: '?ngModel',
+        link   : function (scope, element, attrs, ngModel) {
+            var form = scope[attrs.name];
+            console.log('validateForm', arguments, form);
+        }};
+});
+
+App.directive('validateField', function () {
+    var VALIDATE = {
+        required : '必填！',
+        number   : "必须为数字！",
+        minlength: '太短！',
+        maxlength: '太长！',
+        min      : '太小！',
+        max      : '太大！',
+        more     : '太多！',
+        email    : 'Email无效！',
+        username : '有效字符为汉字、字母、数字、下划线，以汉字或小写字母开头！',
+        minname  : '长度应大于5字节，一个汉字3字节！',
+        maxname  : '长度应小于15字节，一个汉字3字节！',
+        repasswd : '密码不一致！',
+        url      : 'URL无效！',
+        tag      : '标签错误，不能包含“,”、“，”和“、”'
+    };
+    return {
+        require: '?ngModel',
+        link   : function (scope, element, attrs, ngModel) {
+            console.log('validateField', arguments);
+
+            var validateFn = function () {
+                angular.forEach(ngModel.$error, function (value, key) {
+                    console.log(VALIDATE[key]);
+                });
+            };
+
+            /**
+             * ngModel 值改变,进行验证
+             */
+//            scope.$watch(attrs.ngModel, validateFn);
+
+            element.bind('keyup', validateFn);
+        }};
+});
