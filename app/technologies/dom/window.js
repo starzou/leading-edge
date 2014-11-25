45aca00ce9fe548432faded60ce12c44b5dc9a27
starzou <starzou@126.com>
2014-11-25 20:56:15 +0800

    window 对象研究

---
+++ app/technologies/dom/window.js
@@ -0,0 +1,16 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ * @class window
+ * @description
+ * @time 2014-11-25 20:53
+ * @author StarZou
+ **/
+'use strict';
+
+console.log(window);
