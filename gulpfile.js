d5fdd2d9971bb8737738707875a3ab7e86a12bbd
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-11-28 10:28:19 +0800

    use gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ *@class gulpfile.js
+ *@description gulp 配置文件
+ *@time 2014-11-28 10:12
+ *@author StarZou
+ **/
+
+var gulp = require('gulp'),
+    uglify = require('gulp-gulp-uglify');
+
+/**
+ * 压缩js
+ */
+gulp.task('minify', function () {
+    gulp.src('js/app/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+
+/**
+ * 默认任务 :
+ * 使用 命令行: gulp运行
+ */
+gulp.task('default', ['minify']);
