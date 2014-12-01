2db3e2c39f9e19cd9f188abca32d51ffc54f6a9e
starzou <starzou@126.com>
2014-12-01 22:30:15 +0800

    angular 研究

--- app/technologies/angular/app2.js
+++ app/technologies/angular/app2.js
@@ -13,7 +13,32 @@ App.run(['$rootScope', function ($rootScope) {
 }]);
 
 App.controller('AppCtrl', ['$scope', function ($scope) {
+    /**
+     * 监听作用域事件
+     */
+    $scope.$on('eventTriggered', function (event) {
+        console.log(event);
+    });
+}]);
 
+App.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.$on('eventTriggered', function (event) {
+        console.log(event);
+    });
+
+    /**
+     * 向上传播
+     */
+    $scope.emit = function () {
+        $scope.$emit('eventTriggered', {date: Date.now()});
+    };
+
+    /**
+     * 向下传播
+     */
+    $scope.broadcast = function () {
+        $scope.$parent.$broadcast('eventTriggered', {date: Date.now()});
+    };
 }]);
 
 App.directive('sayHello', [function () {
