d0ca70f9288c659f34b68123d39d5579b4f972d8
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-01 17:06:01 +0800

    add angular-strap model example

--- app/technologies/angular-strap/app.js
+++ app/technologies/angular-strap/app.js
@@ -8,20 +8,25 @@
 (function (window, document, undefined) {
     'use strict';
 
-    var App = angular.module('App', ['mgcrea.ngStrap']);
+    var App = angular.module('App', ['ngAnimate', 'mgcrea.ngStrap']);
 
     App.run(['$rootScope', function ($rootScope) {
         $rootScope.title = 'angular-strap 研究';
     }]);
 
-    App.controller('AppCtrl', ['$scope', '$modal', function ($scope, $modal) {
+    App.controller('AppCtrl', ['$scope', '$modal', '$alert', function ($scope, $modal, $alert) {
 
-        var model = $modal({title: 'hello', content: 'Hello angular-strap!', animation: 'am-fade', show: false});
+        var model = $modal({title: 'hello', content: 'Hello angular-strap!', animation: 'am-fade-and-slide-top', show: false});
 
         $scope.showModal = function () {
             model.$promise.then(model.show);
         };
 
+        var myAlert = $alert({title: 'Holy guacamole!', content: 'Best check yo self, you\'re not looking too good.', placement: 'top', type: 'info', show: false, animation: 'am-fade-and-slide-top', duration: 3});
+
+        $scope.showAlert = function () {
+            myAlert.show();
+        };
     }]);
 
 })(window, document);
