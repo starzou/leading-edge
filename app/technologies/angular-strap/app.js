05a329f3ef7f2751b4ca067cc8c1960b95380c0e
starzou <starzou@126.com>
2014-12-04 22:19:46 +0800

    angular 研究

--- app/technologies/angular-strap/app.js
+++ app/technologies/angular-strap/app.js
@@ -71,4 +71,93 @@
         };
     }]);
 
+    /**
+     * 表单验证指令, 该指令在 form元素上使用
+     */
+    App.directive('validateForm', ['$tooltip', function ($tooltip) {
+        var me = {
+            lastTooltips: [],
+            validateFn  : function ($element, ngFormController) {
+                var me = this,
+                    formElement = $element[0], // form元素
+                    formField,
+                    tooltip,
+                    name;
+
+                /**
+                 * 先移除上次的提示
+                 */
+                angular.forEach(me.lastTooltips, function (tooltip) {
+                    tooltip.hide();
+                })
+
+                /**
+                 * 进行验证, 显示错误提示
+                 */
+                angular.forEach(ngFormController.$error, function (ngModelControllers, type) {
+
+                    angular.forEach(ngModelControllers, function (ngModelController) {
+                        name = ngModelController.$name;
+                        formField = formElement.querySelector('[name="' + name + '"]');
+
+                        /**
+                         * 显示提示
+                         */
+                        tooltip = $tooltip(angular.element(formField), {
+                            title    : type,
+                            trigger  : 'manual',
+                            placement: 'right',
+                            show     : true
+                        });
+
+                        me.lastTooltips.push(tooltip);
+                    });
+
+                });
+
+
+            }
+        };
+
+        return {
+            restrict: 'A',
+            compile : function ($element, $attr) {
+
+                /**
+                 * 为表单添加了ng-model属性的字段 绑定 name,
+                 * 以便于angular 为其创建 ngModelController, 实现字段校验
+                 */
+
+                var formElement = $element[0],
+                    ngModelAttributeName = 'ng-model',
+                    formFields = formElement.querySelectorAll('[ ' + ngModelAttributeName + ']'),
+                    formField,
+                    index;
+
+                for (index = 0; index < formFields.length; index++) {
+                    formField = formFields[index];
+                    formField.name = formField.getAttribute(ngModelAttributeName); // 设置 表单字段的name 为 ng-model属性值
+                }
+
+                /**
+                 * 使原生浏览器的校验无效
+                 */
+                formElement.setAttribute('novalidate', 'novalidate');
+
+
+                return function ($scope, $element, $attr) {
+                    var ngFormController = $scope[$attr.name]; // 取得ngFormController
+
+                    $element.on('submit', function (event) {
+                        me.validateFn($element, ngFormController)
+                    });
+
+                    $element.on('reset', function (event) {
+
+                    });
+                };
+            }
+        }
+    }]);
+
 })(window, document);
