ed94b3d1980d624d583735616e0d6d5fcff9a84b
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-04 18:55:04 +0800

    angular 表单验证

--- app/technologies/angular-strap/app.js
+++ app/technologies/angular-strap/app.js
@@ -14,19 +14,39 @@
         $rootScope.title = 'angular-strap 研究';
     }]);
 
-    App.controller('AppCtrl', ['$scope', '$modal', '$alert', function ($scope, $modal, $alert) {
+    App.controller('AppCtrl', ['$scope', '$modal', '$alert', '$aside', function ($scope, $modal, $alert, $aside) {
 
-        var model = $modal({title: 'hello', content: 'Hello angular-strap!', animation: 'am-fade-and-scale', placement: 'center', show: false});
+        var model = $modal({
+            title    : 'hello',
+            content  : 'Hello angular-strap!',
+            animation: 'am-fade-and-scale',
+            placement: 'center',
+            show     : false
+        });
 
         $scope.showModal = function () {
             model.$promise.then(model.show);
         };
 
-        var myAlert = $alert({title: 'Holy guacamole!', content: 'Best check yo self, you\'re not looking too good.', placement: 'top', type: 'info', show: false, animation: 'am-fade-and-slide-top', duration: 3});
+        var myAlert = $alert({
+            title    : 'Holy guacamole!',
+            content  : 'Best check yo self, you\'re not looking too good.',
+            placement: 'top',
+            type     : 'info',
+            show     : false,
+            animation: 'am-fade-and-slide-top',
+            duration : 3
+        });
 
         $scope.showAlert = function () {
             console.log('show alert...');
             myAlert.show();
+        };
+
+        var myAside = $aside({title: 'My Title', content: 'My Content', show: false});
+
+        $scope.showAside = function () {
+            myAside.show();
         };
     }]);
 
