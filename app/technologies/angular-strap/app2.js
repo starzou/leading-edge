df8cf1558083153e538b31f3f2d499c4588f79a2
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-05 10:36:28 +0800

    日期控件使用

---
+++ app/technologies/angular-strap/app2.js
@@ -0,0 +1,24 @@
+/**
+ * @class app2
+ * @description
+ * @time 2014-12-05 10:14
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+
+    var App = angular.module('App', ['ngAnimate', 'mgcrea.ngStrap']);
+
+    App.config(['$datepickerProvider', function ($datepickerProvider) {
+        console.log($datepickerProvider);
+    }]);
+
+    App.run(['$rootScope', function ($rootScope) {
+        $rootScope.title = 'angular-strap 研究2';
+    }]);
+
+    App.controller('AppCtrl', ['$scope', function ($scope) {
+
+    }]);
+
+})(window, document);
