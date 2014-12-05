b6008a308da8bba1bb3c8c3a04d07d6b79d3ad97
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-05 18:57:39 +0800

    添加 lodash

---
+++ app/technologies/lodash/app.js
@@ -0,0 +1,12 @@
+/**
+ * @class app
+ * @description
+ * @time 2014-12-05 18:56
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+
+    console.log(_.compact([0, 1, false, 2, '', 3]));
+
+})(window, document);
