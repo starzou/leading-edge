102e6820f8c9cc0094a8795fc31d6debb99cf83e
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-12 12:48:50 +0800

    添加 dragdro

--- app/technologies/drag/app.js
+++ app/technologies/drag/app.js
@@ -14,3 +14,76 @@ App.run(['$rootScope', function ($rootScope) {
 App.controller('AppCtrl', ['$scope', function ($scope) {
 
 }]);
+
+
+var target;
+
+App.factory('dragDrop', function () {
+    var me = {
+        bindDragStart: function ($element) {
+            $element.on('dragstart', function (event) {
+                me.target = event.target;
+            });
+
+            return me;
+        },
+
+        bindDragOver: function ($element) {
+            $element.on('dragover', function (event) {
+                event.preventDefault();
+            });
+            return me;
+        },
+
+        bindDrop: function ($element) {
+            $element.on('drop', function (event) {
+                event.preventDefault();
+                event.target.appendChild(me.target);
+            });
+            return me;
+        }
+    };
+
+    return me;
+});
+
+App.directive('uiDrag', [function () {
+    return {
+        restrict: 'A',
+        scope   : true,
+        compile : function ($element, $attr) {
+            $element.attr('draggable', true);// 元素设为可拖动
+            return function ($scope, $element, $attr) {
+
+                $element.on('dragstart', function (event) {
+                    target = event.target;
+                });
+
+                $element.on('dragover', function (event) {
+                    event.preventDefault();
+                });
+            };
+        }
+    }
+}]);
+
+App.directive('uiDrop', [function () {
+    return {
+        restrict: 'A',
+        scope   : true,
+        compile : function ($element, $attr) {
+            return function ($scope, $element, $attr) {
+
+                $element.on('drop', function (event) {
+                    event.preventDefault();
+                    event.target.appendChild(target);
+                });
+
+
+                $element.on('dragover', function (event) {
+                    event.preventDefault();
+                });
+            };
+        }
+    }
+}]);
