9e1565405bcfab369f75ff06c429017169b72820
starzou <starzou@126.com>
2014-12-15 21:50:17 +0800

    angular 研究3 angular.element

--- app/technologies/angular/app3.js
+++ app/technologies/angular/app3.js
@@ -21,14 +21,33 @@
 
         console.log($rootElement);
 
+        /**
+         * angular.bind 示例
+         * @param word
+         */
         var fn = function (word) {
-            console.log('%s say %s', this.name, word);
-        };
-
-        var warpFn = angular.bind({name: 'StarZou'}, fn, 'hello');
+                console.log('%s say %s', this.name, word);
+            },
+            warpFn = angular.bind({name: 'StarZou'}, fn, 'hello');
 
         $scope.bind = function () {
             warpFn();
+        };
+
+
+        /**
+         * angular.element 示例
+         */
+        $scope.element = function () {
+            var $html = angular.element(document.documentElement),
+                $body = angular.element(document.body),
+                $h1 = angular.element('<h1>hello</h1>');
+
+            $body.append($h1);
+
+            console.log($html.scope());
+            console.log($body);
+            console.log($h1);
         };
 
     }]);
