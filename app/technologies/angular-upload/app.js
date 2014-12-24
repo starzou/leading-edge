3be05def716ebbf13ed4d7d8ada1a528121bfe01
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-24 12:55:59 +0800

    添加 angular-upload

---
+++ app/technologies/angular-upload/app.js
@@ -0,0 +1,26 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ * @class app
+ * @description
+ * @time 2014-12-14 20:24
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+
+    var App = angular.module('app', []);
+
+    App.run(['$rootScope', function ($rootScope) {
+        $rootScope.title = 'angular-upload 上传示例';
+    }]);
+
+    App.controller('AppController', ['$scope', '$compile', '$parse', function ($scope, $compile, $parse) {
+        console.log($scope);
+    }]);
+})(window, document);
