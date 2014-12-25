d3145ddeadedd1e76f30b64d134800129759aaf3
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-25 16:28:45 +0800

    添加 ui-router 示例

--- app/technologies/ui-router/app.js
+++ app/technologies/ui-router/app.js
@@ -16,11 +16,24 @@
 
     var App = angular.module('app', ['ui.router']);
 
+    App.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {url: '/', template: '<h1>home</h1>'})
+            .state('page1', {url: '/page1', template: '<h1>page1</h1>'})
+            .state('page2', {url: '/page2', template: '<h1>page2</h1>'})
+            .state('page3', {url: '/page3', templateUrl: 'page3.html'});
+    }]);
+
     App.run(['$rootScope', function ($rootScope) {
         $rootScope.title = 'ui-router 示例';
     }]);
 
     App.controller('AppController', ['$scope', function ($scope) {
         console.log($scope);
+    }]);
+
+    App.controller('Page3Controller', ['$scope', function ($scope) {
+        $scope.title = 'this is page3';
     }]);
 })(window, document);
