4442ac6dae74acc56371d7756dcfd57fa90753bc
tongyuan.zou <tongyuan.zou@shuyun.com>
2015-01-15 18:51:57 +0800

    angularTreeview

--- app/technologies/angular-about/app.js
+++ app/technologies/angular-about/app.js
@@ -14,12 +14,33 @@
 (function (window, document) {
     'use strict';
 
-    var App = angular.module('app', ['colorpicker.module']);
+    var App = angular.module('app', ['colorpicker.module', 'angularTreeview']);
 
     App.controller('AppController', ['$scope', '$compile', '$parse', function ($scope, $compile, $parse) {
         $scope.title = 'Angular 相关研究';
 
         console.log('AppController...');
+
+        $scope.treedata =
+            [
+                {
+                    "label": "User", "id": "role1", "children": [
+                    {"label": "subUser1", "id": "role11", "children": []},
+                    {
+                        "label": "subUser2", "id": "role12", "children": [
+                        {
+                            "label": "subUser2-1", "id": "role121", "children": [
+                            {"label": "subUser2-1-1", "id": "role1211", "children": []},
+                            {"label": "subUser2-1-2", "id": "role1212", "children": []}
+                        ]
+                        }
+                    ]
+                    }
+                ]
+                },
+                {"label": "Admin", "id": "role2", "children": []},
+                {"label": "Guest", "id": "role3", "children": []}
+            ];
     }]);
 
 })(window, document);
