c902ff51414e719e71109fb082bc7e92c28b24ea
starzou <starzou@126.com>
2015-01-30 20:23:28 +0800

    jquery

---
+++ app/technologies/jquery/js/1.js
@@ -0,0 +1,18 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ * @class 1
+ * @description
+ * @time 2015-01-30 20:21
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+    console.log('jquery 研究...');
+
+})(window, document);
