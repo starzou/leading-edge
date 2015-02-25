4d61f1a8d7a53fd64c214b39d98797981e66dc23
tongyuan.zou <tongyuan.zou@shuyun.com>
2015-02-25 11:13:11 +0800

    深入学习 js

---
+++ app/technologies/javascript/thorough.js
@@ -0,0 +1,15 @@
+/**
+ * @class thorough
+ * @description 深入学习 js
+ * @time 2015-02-25 11:08
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+
+    var tilteElement = document.getElementById('tilte');
+    var name = '深入学习 js';
+
+    tilteElement.textContent = name;
+
+})(window, document);
